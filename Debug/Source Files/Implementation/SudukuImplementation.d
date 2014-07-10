00e539848a5e3ca980b47dfaa31c328723aa17a7
Przemek Kura <kuraprzemek@gmail.com>
2014-07-10 21:04:25 +0200

    change gitignore file

--- Debug/Source Files/Implementation/SudukuImplementation.d
+++
@@ -1,8 +0,0 @@
-Source\ Files/Implementation/SudukuImplementation.d: \
- ../Source\ Files/Implementation/SudukuImplementation.cpp \
- ../Source\ Files/Implementation/../Interface/sudukuheader.h \
- ../Source\ Files/Implementation/../Interface/datastructue.h
-
-../Source\ Files/Implementation/../Interface/sudukuheader.h:
-
-../Source\ Files/Implementation/../Interface/datastructue.h:
